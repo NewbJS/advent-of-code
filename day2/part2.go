2e758654b0b898602c16a28a0512b38fe56d0665
Solar <solartruth101@gmail.com>
2020-12-24 10:02:20 -0500

    Did day 2 part 2

---
+++ day2/part2.go
@@ -0,0 +1,54 @@
+package main
+
+import (
+	"fmt"
+	"io/ioutil"
+	"strconv"
+	"strings"
+)
+
+func main() {
+	data, err := ioutil.ReadFile("c:\\Users\\solar\\downloads\\passwords.txt")
+	if err != nil {
+		fmt.Println("File reading error", err)
+		return
+	}
+
+	doThing(strings.Split(string(data), "\n"))
+
+}
+
+var passwordsFound int32
+
+func doThing(slice []string) {
+	for sIdx, el := range slice {
+		if sIdx < len(slice)-1 {
+			splitAtDash := strings.Split(el, "-")
+			f := strings.Split(splitAtDash[1], " ")
+			lowestPoint, _ := strconv.ParseInt(splitAtDash[0], 10, 64)
+			highestPoint, _ := strconv.ParseInt(f[0], 10, 64)
+
+			z := strings.Split(f[1], "")
+			letterToSearchFor := z[0]
+
+			x := strings.Split(f[2], "")
+			amountFound := 0
+
+			for idx, n := range x {
+				idx64 := int64(idx)
+				if n == letterToSearchFor && idx64+1 == lowestPoint {
+					amountFound++
+
+				} else if n == letterToSearchFor && idx64+1 == highestPoint {
+					amountFound++
+				}
+			}
+
+			if amountFound == 1 {
+				passwordsFound++
+			}
+
+		}
+	}
+	fmt.Println("Found", passwordsFound, "valid passwords!")
+}
